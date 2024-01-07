5606d7314d20b9704572694821c3c4b8f2463b52
Inga Kononova <inga.kononova@gmail.com>
2024-01-07 15:20:40 +0200

    Comments for abbreviate-a-two-word-name.js

--- 8kyu/abbreviate-a-two-word-name.js
+++ 8kyu/abbreviate-a-two-word-name.js
@@ -10,8 +10,14 @@ patrick feeney => P.F */
 
 // Solution:
 function abbrevName(name){
-  const indexOfFirstCharOfSurname = (name.indexOf(" ")+1);
-  const firstCharOfSurname = name[indexOfFirstCharOfSurname];
-  const firstCharOfName = name.charAt(0);
-  return firstCharOfName.concat(".", firstCharOfSurname).toUpperCase();
+  const indexOfFirstCharOfSurname = (name.indexOf(" ")+1); //Got the INDEX of the first character of the surname
+  const firstCharOfSurname = name[indexOfFirstCharOfSurname]; // Got the first character of the surname
+  const firstCharOfName = name.charAt(0); // Got the first character of the name
+  return firstCharOfName.concat(".", firstCharOfSurname).toUpperCase(); // Concatenate both characters into one string, with dot in the middle
 }
+
+/* Shorter option:
+function abbrevName(name){
+	return name[0].toUpperCase() + "." + name[name.indexOf(" ")+1].toUpperCase();
+}
+*/
