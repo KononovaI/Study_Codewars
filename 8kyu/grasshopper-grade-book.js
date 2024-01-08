1649de5a1c6a0ab0eeb6158a76d7fa67447df1c3
Inga Kononova <inga.kononova@gmail.com>
2024-01-08 19:31:07 +0200

    grasshopper-grade-book.js

--- 8kyu/grasshopper-grade-book.js
+++ 8kyu/grasshopper-grade-book.js
@@ -24,3 +24,17 @@ function getGrade(s1, s2, s3) {
     return "F";
   }
 }
+
+// If Else can be replaced with switch:
+/* switch (true) {
+	case score >= 90 && score <= 100:
+		return "A";
+	case score >= 80 && score < 90:
+		return "B";
+	case score >= 70 && score < 80:
+		return "C";
+	case score >= 60 && score < 70:
+		return "D";
+	default:
+		return "F";
+} */
