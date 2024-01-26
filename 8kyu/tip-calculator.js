1bcd62ae3e52a8380f8fd0ac37b0cae0a2767549
Inga Kononova <inga.kononova@gmail.com>
2024-01-26 16:59:46 +0200

    Added tip.calculator.js

---
+++ 8kyu/tip-calculator.js
@@ -0,0 +1,39 @@
+/* Complete the function, which calculates how much you need to tip based on the total amount of the bill and the service.
+
+You need to consider the following ratings:
+
+Terrible: tip 0%
+Poor: tip 5%
+Good: tip 10%
+Great: tip 15%
+Excellent: tip 20%
+The rating is case insensitive (so "great" = "GREAT"). If an unrecognised rating is received, then you need to return:
+
+"Rating not recognised" in Javascript, Python and Ruby...
+...or null in Java
+...or -1 in C#
+Because you're a nice person, you always round up the tip, regardless of the service. */
+
+// Solution:
+function calculateTip(amount, rating) {
+  let result;
+  switch (rating.toUpperCase()) {
+      case 'TERRIBLE':
+        result = 0;
+        break;
+      case 'POOR':
+        result = amount * 0.05;
+        break;
+      case 'GOOD':
+        result = amount * 0.1;
+        break;
+      case 'GREAT':
+        result = amount * 0.15;
+        break;
+      case 'EXCELLENT':
+        result = amount * 0.2;
+        break;
+      default: console.log("Rating not recognised");
+  }
+  return Math.ceil(result);
+}
