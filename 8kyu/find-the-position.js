33179ca8d7913ced2b64c8df7675b835d1a4493a
Inga Kononova <inga.kononova@gmail.com>
2024-02-02 14:07:11 +0200

    Comment for find-the-positions.js

--- 8kyu/find-the-position.js
+++ 8kyu/find-the-position.js
@@ -9,3 +9,9 @@ function position(letter){
   let result = letter.toUpperCase().charCodeAt() -'A'.charCodeAt(0) +1;
 return `Position of alphabet: ${result}`
 }
+
+/* Shortest:
+function position(letter){
+  let result = letter.toUpperCase().charCodeAt()-64; // Because the result of the charCodeAt() for "A" is 65
+return `Position of alphabet: ${result}`
+} */
