128a558b483ca11ec4325db3372c3e3777d02930
Inga Kononova <inga.kononova@gmail.com>
2024-11-28 20:20:42 +0200

    Added remove-anchor-from-url.js

---
+++ 7kyu/remove-anchor-from-url.js
@@ -0,0 +1,10 @@
+/* Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+Examples
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1" */
+
+function removeUrlAnchor(url){
+  let index = url.indexOf("#");
+  return index <0 ? url : url.slice(0, index);
+}
