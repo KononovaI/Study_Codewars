c899802a099f8389c096b55953e93346023e61f4
Inga Kononova <inga.kononova@gmail.com>
2025-01-08 19:46:08 +0200

    Added comments for conditional-statement--switch.js

--- 8kyu/conditional-statement--switch.js
+++ 8kyu/conditional-statement--switch.js
@@ -35,3 +35,16 @@ function howManydays(month){
   }
   return days;
 }
+
+/* Other (shorter solution): */
+
+function howManydays(month){
+  switch (month){
+     case 2: return 28
+     case 4:
+     case 6:
+     case 9:
+     case 11: return 30
+  }
+  return 31
+}
