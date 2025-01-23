9e6470d46dbc8e19c16472eca34e85d7cae4903c
Inga Kononova <inga.kononova@gmail.com>
2025-01-23 15:15:24 +0200

    Added bartender-drinks.js

---
+++ bartender-drinks.js
@@ -0,0 +1,39 @@
+/* Complete the function that receives as input a string, and produces outputs according to the following table:
+
+Input	Output
+"Jabroni"	"Patron Tequila"
+"School Counselor"	"Anything with Alcohol"
+"Programmer"	"Hipster Craft Beer"
+"Bike Gang Member"	"Moonshine"
+"Politician"	"Your tax dollars"
+"Rapper"	"Cristal"
+anything else	"Beer"
+Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer".
+
+Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars". */
+
+function getDrinkByProfession(param){
+  let result;
+  switch(param.toLowerCase()){
+    case "Jabroni".toLowerCase():
+      result = "Patron Tequila";
+      break;
+    case "School Counselor".toLowerCase():
+      result = "Anything with Alcohol";
+      break;
+    case "Programmer".toLowerCase():
+      result = "Hipster Craft Beer";
+      break;
+    case "Bike Gang Member".toLowerCase():
+      result = "Moonshine";
+      break;
+    case "Politician".toLowerCase():
+      result = "Your tax dollars";
+      break;
+    case "Rapper".toLowerCase():
+      result = "Cristal";
+      break;
+    default:
+      result = "Beer";}
+  return result;
+}
